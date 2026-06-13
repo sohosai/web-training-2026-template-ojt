272aba3f99f3ff5b673852e8a907450ad58f8804
Sause-Source <126079755+Sause-Source@users.noreply.github.com>
2026-06-13 15:35:04 +0900

    コメントのいいね機能UIを実装

--- frontend/src/routes/threads/$threadName.tsx
+++ frontend/src/routes/threads/$threadName.tsx
@@ -64,6 +64,15 @@ function ThreadPage() {
               <p style={{ margin: "0.25rem 0 0", whiteSpace: "pre-wrap" }}>
                 {m.message}
               </p>
+              <div style={{ margin: "0.25rem 0 0", display: "flex", alignItems: "center" }}>
+                <svg 
+              xmlns="http://www.w3.org/2000/svg" width="17" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" ><path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2a3.13 3.13 0 0 1 3 3.88Z"/><path d="M7 10v12"/></svg>
+                <span style={{ marginLeft: "0.10rem" , fontSize: "15px"}}>{"10000"}</span>
+                <svg style={{ marginLeft: "0.50rem", marginRight: "0" }} 
+              xmlns="http://www.w3.org/2000/svg" width="17" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" ><path d="M9 18.12 10 14H4.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 6.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22a3.13 3.13 0 0 1-3-3.88Z"/><path d="M17 14V2"/></svg>
+                <span style={{ marginLeft: "0.10rem" , fontSize: "15px"}}>{"0"}</span>
+              </div>
+              
             </li>
           ))}
         </ul>
